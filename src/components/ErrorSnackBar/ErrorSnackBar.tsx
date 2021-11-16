fd2ab9247214a4faeb3631a4474befcf3fdaab58
vladimirmartynyuk <martynyukvm@mail.ru>
2021-11-16 23:27:39 +0300

    ErrorSnackBar.tsx refactor

--- src/components/ErrorSnackBar/ErrorSnackBar.tsx
+++ src/components/ErrorSnackBar/ErrorSnackBar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, {AlertProps} from '@mui/material/Alert';
 
@@ -11,13 +10,11 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 ) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
-
+//error | warning | info | success
 export const ErrorSnackBar = () => {
 
-    const [open, setOpen] = React.useState(false)
-    const handleClick = () => {
-        setOpen(true)
-    }
+    const [open, setOpen] = React.useState(true)
+
     const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason === 'clickaway') {
             return
@@ -26,17 +23,9 @@ export const ErrorSnackBar = () => {
     };
 
     return (<Stack spacing={2} sx={{width: '100%'}}>
-        <Button variant="outlined" onClick={handleClick}>
-            Open success snackbar
-        </Button>
-        <Snackbar anchorOrigin={{vertical:'top',horizontal:'right'}} open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success" sx={{width: '100%'}}>
-                This is a success message!
-            </Alert>
+        <Snackbar anchorOrigin={{vertical: 'bottom', horizontal: 'center'}} open={open} autoHideDuration={6000}
+                  onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{width: '100%'}} children={'This is a successmessage'}/>
         </Snackbar>
-        <Alert severity="error">This is an error message!</Alert>
-        <Alert severity="warning">This is a warning message!</Alert>
-        <Alert severity="info">This is an information message!</Alert>
-        <Alert severity="success">This is a success message!</Alert>
     </Stack>)
 }

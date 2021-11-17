cbe0dd6f901ad6160bc05ad168134055f2967676
vladimirmartynyuk <martynyukvm@mail.ru>
2021-11-18 01:05:52 +0300

    login-reducer.ts added

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,13 +3,15 @@ import { todolistsReducer } from '../state/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from "redux-thunk";
 import {appReducer} from "./app-reducer";
+import {loginReducer} from "../components/Login/login-reducer";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
-    app:appReducer
+    app:appReducer,
+    auth:loginReducer
 })
 // непосредственно создаём store
 export const store = createStore(rootReducer,applyMiddleware(thunk));

05954881dc5db3e1dfbe000aab6fd68357476960
vladimirmartynyuk <martynyukvm@mail.ru>
2021-11-18 00:27:27 +0300

    Login validate form added, need to create reducer

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -7,10 +7,30 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useFormik} from "formik";
 
 export const Login = () => {
+    const formik = useFormik({
+        validate:(values)=>{
+            if(values.email.length === 0){
+                return {email:'Email is required'}
+            }
+            if(values.password.length === 0){
+                return {password:'Password is required'}
+            }
+        },
+        initialValues: {
+            email: '',
+            password:'',
+            rememberMe:false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    });
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
+            <form onSubmit={formik.handleSubmit}>
             <FormControl>
                 <FormLabel>
                     <p>To log in get registered
@@ -23,16 +43,18 @@ export const Login = () => {
                     <p>Password: free</p>
                 </FormLabel>
                 <FormGroup>
-                    <TextField label="Email" margin="normal"/>
-                    <TextField type="password" label="Password"
+                    <TextField error={!!formik.errors.email} helperText={formik.errors.email ? formik.errors.email : null} label="Email" margin="normal" {...formik.getFieldProps('email')}/>
+                    <TextField error={!!formik.errors.password} helperText={formik.errors.password ? formik.errors.password : null}  type="password" label="Password"
                                margin="normal"
+                               {...formik.getFieldProps('password')}
                     />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
+                    <FormControlLabel label={'Remember me'} control={<Checkbox checked={formik.values.rememberMe} {...formik.getFieldProps('rememberMe')} name={'rememberMe'}/>}/>
                     <Button type={'submit'} variant={'contained'} color={'primary'}>
                         Login
                     </Button>
                 </FormGroup>
             </FormControl>
+            </form>
         </Grid>
     </Grid>
 }

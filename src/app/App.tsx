2ae91c288012e191431aa043b6c139dece030a46
vladimirmartynyuk <martynyukvm@mail.ru>
2021-11-18 21:59:59 +0300

    Appbar logOut added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {Menu} from '@mui/icons-material';
 import {TodolistsList} from "../pages/Todolists/TodolistsList";
-import {LinearProgress} from "@mui/material";
+import {Button, LinearProgress} from "@mui/material";
 import {ErrorSnackBar} from "../components/ErrorSnackBar/ErrorSnackBar";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./store";
@@ -15,13 +15,13 @@ import {InitialAppStatuses, setAppInitializedChecker} from "./app-reducer";
 import {BrowserRouter, Route} from "react-router-dom";
 import {Login} from "../components/Login/Login";
 
-
 function App() {
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(setAppInitializedChecker())
-    },[])
+    }, [])
     const status = useSelector<AppRootStateType, InitialAppStatuses>(state => state.app.status)
+    const isAuth = useSelector<AppRootStateType,boolean>(state=>state.auth.isAuth)
     const init = useSelector<AppRootStateType, boolean>(state => state.app.initialized)
     if (!init) {
         return <div style={{
@@ -29,27 +29,39 @@ function App() {
             display: 'flex',
             width: '100%',
             justifyContent: 'center',
-            flexDirection:'column',
+            flexDirection: 'column',
             alignItems: 'center'
         }}>
             <Typography variant="h2" component="div" gutterBottom>
                 Todolist
             </Typography>
-            <div style={{width:'30%'}}><LinearProgress color="inherit"/></div>
+            <div style={{width: '30%'}}><LinearProgress color="inherit"/></div>
         </div>
     }
-
 
     return (<BrowserRouter>
             <div className="App">
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <Menu/>
-                        </IconButton>
-                        <Typography variant="h6">
-                            Todolist
-                        </Typography>
+                        <div style={{
+                            display: 'flex',
+                            width: '100%',
+                            justifyContent: 'space-between',
+                            height: '100%'
+                        }}>
+                            <div style={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}>
+                                <IconButton edge="start" color="inherit" aria-label="menu">
+                                    <Menu/>
+                                </IconButton>
+                                <Typography variant="h6">
+                                    Todolist
+                                </Typography>
+                            </div>
+                            {isAuth && <Button color="inherit">Log Out</Button>}
+                        </div>
                     </Toolbar>
                     {status === 'loading' && <LinearProgress/>}
                 </AppBar>

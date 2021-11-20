992e30243f9a921fd4b9a82892da239c970cba63
vladimirmartynyuk <martynyukvm@mail.ru>
2021-11-20 20:23:51 +0300

    Redux-toolkit works, ready to change action-creators

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,31 @@
-import { tasksReducer } from '../state/tasks-reducer';
-import { todolistsReducer } from '../state/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {tasksReducer} from '../state/tasks-reducer';
+import {todolistsReducer} from '../state/todolists-reducer';
+import { combineReducers} from 'redux';
 import thunk from "redux-thunk";
 import {appReducer} from "./app-reducer";
 import {loginReducer} from "../components/Login/login-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
-    app:appReducer,
-    auth:loginReducer
+    app: appReducer,
+    auth: loginReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer,applyMiddleware(thunk));
+//export const store = createStore(rootReducer,applyMiddleware(thunk));
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware:(getDefaultMiddleware) =>
+        getDefaultMiddleware()
+            .prepend(
+                thunk,
+            )
+})
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
-
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
